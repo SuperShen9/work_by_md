54e873e99ec70f8234fd47f88bfd82eaba8e84bc
Super <you@example.com>
2018-10-26 14:37:17 +0800

    1026

---
+++ 备用code文件/于_每日数据指标/3，金币分类.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+# author：Super.Shen
+
+import pandas as pd
+from Func import gb, ri_y, nian, yue, df_cut, ri_y2
+
+pd.set_option('expand_frame_repr', False)
+pd.set_option('display.max_rows', 1000)
+import warnings
+
+warnings.filterwarnings('ignore')
+
+try:
+    # 读取数据
+    df = pd.read_excel('C:\\Users\Administrator\Desktop\每日数据分析\金币分类.xlsx')
+    df_map = pd.read_excel('C:\\Users\Administrator\Desktop\map.xlsx')
+except FileNotFoundError:
+    print('\n缺少运行数据，请先下载……')
+    exit()
+
+# 金币消耗-透视
+df2 = df[df.columns[-3:]]
+df2.dropna(axis=0, how='any', inplace=True)
+df2 = pd.pivot_table(df2, values='数值2', index='时间2', columns='原因2')
+del df2['单局结算']
+df2.reset_index(inplace=True)
+df2['时间2'] = df2['时间2'].apply(lambda x: str(x)[:10])
+
+
+
+# 金币产出-透视
+df = df[df.columns[:3]]
+df_map = df_map[['原因', 'jinbi']]
+df_map.dropna(inplace=True)
+
+# 合并匹配表
+df = pd.merge(left=df, right=df_map, on='原因', how='left')
+
+df = df.groupby(['时间', 'jinbi'])['数值'].sum()
+
+df = pd.DataFrame(df)
+df.reset_index(inplace=True)
+
+df = pd.pivot_table(df, values='数值', index='时间', columns='jinbi')
+
+df.reset_index(inplace=True)
+
+df = df[['时间', '用户充值', '系统赠送', '兑换礼物', '领取邮件']]
+df['时间'] = df['时间'].apply(lambda x: str(x)[:10])
+
+# print(df)
+# print('-' * 100)
+# print(df2)
+# exit()
+
+writer = pd.ExcelWriter('C:\\Users\Administrator\Desktop\表格提取源\金币分类_OUT.xlsx')
+df.tail(2).to_excel(writer, sheet_name='金币产出', index=False)
+df2.tail(2).to_excel(writer, sheet_name='金币消耗', index=False)
+writer.save()
---
+++ 备用code文件/于_每日数据指标/6，我要赚钱.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# author：Super.Shen
+
+import pandas as pd
+import numpy as np
+from Func import gb, ri_y, nian, yue, df_cut, ri_y2
+from Func import du_old_excel
+pd.set_option('expand_frame_repr', False)
+pd.set_option('display.max_rows', 1000)
+import warnings
+
+warnings.filterwarnings('ignore')
+
+df=du_old_excel('我要赚钱')
+
+df['奖励一状态'].replace({'已完成': 1, '未完成': 0}, inplace=True)
+df['奖励二状态'].replace({'已完成': 1, '未完成': 0}, inplace=True)
+df['奖励三状态'].replace({'已完成': 1, '未完成': 0}, inplace=True)
+df['time'] = df['关系建立时间'].apply(lambda x: x.split(' ')[0])
+
+df1 = df.groupby(['time'])['奖励一状态'].sum()
+df2 = df.groupby(['time'])['奖励二状态'].sum()
+df3 = df.groupby(['time'])['奖励三状态'].sum()
+
+df4 = df.groupby(['time']).size()
+
+df = pd.DataFrame([df1, df2, df3, df4]).T
+
+df.reset_index(inplace=True)
+df['day'] = df['time'].apply(lambda x: int(x.split('/')[-1]))
+
+df = df[(df['day'] >= int(ri_y2)) & (df['day'] <= int(ri_y))]
+
+df.rename(columns={'Unnamed 0': '总和'}, inplace=True)
+
+df['一档完成率'] = (df['奖励一状态'] / df['总和']).apply(lambda x: '%.2f%%' % x)
+df['二档完成率'] = (df['奖励二状态'] / df['总和']).apply(lambda x: '%.2f%%' % x)
+df['三档完成率'] = (df['奖励三状态'] / df['总和']).apply(lambda x: '%.2f%%' % x)
+
+df = df[['time', '奖励一状态', '奖励二状态', '奖励三状态', '一档完成率', '二档完成率', '三档完成率','总和']]
+
+df.rename(columns={'奖励一状态': '奖励一完成人数', '奖励二状态': '奖励二完成人数', '奖励三状态': '奖励三完成人数', 'time': '日期'}, inplace=True)
+
+
+df.to_excel('C:\\Users\Administrator\Desktop\表格提取源\我要赚钱_OUT.xlsx', index=False)
